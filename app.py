65b289c28c494b9a50c34c5362ede96c2523c9a0
chahatyadav336 <chahatyadav369.56@gmail.com>
2025-12-28 12:12:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,11 @@ st.title('🏠House Price prediction using ML')
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQdqIasHkDTf5733FK14z5mPQ18VPhg_R_Q&s')
 
 df = pd.read_csv('house_data.csv')
-x = df.iloc[:,:-3]
+X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
-
-
 
 final_X = X
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(final_X)
 
-st.sidebar.title('Select House features:')
-
+st.sidebar.title('Select House features: ')
